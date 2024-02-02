3c4dd30c064a751d07735c722e079562dfe72180
GiftChrisJava <chrisjava77@gmail.com>
2024-02-02 13:47:59 +0200

    created add watch server action

---
+++ app/server-actions/addWatch.js
@@ -0,0 +1,43 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+
+export async function addWatch(formData) {
+  // getting data from the form
+  const model = formData.get("model");
+  const brand = formData.get("brand");
+  const referenceNumber = formData.get("referenceNumber");
+
+  const cookieStore = cookies();
+  const supabase = createClientComponentClient({ cookies: () => cookieStore });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
+
+  // check if user is authenticated
+  if (!user) {
+    console.error("User not authenticated within addWatch server action");
+    return;
+  }
+
+  // insert data into the table
+  const { data, error } = await supabase.from("watches").insert([
+    {
+      model,
+      brand,
+      reference_number: referenceNumber,
+      user_id: user.id,
+    },
+  ]);
+
+  if (error) {
+    console.error("Error inserting data", error);
+    return;
+  }
+
+  // if success
+  revalidatePath("/watch-list");
+
+  return { message: "Success" };
+}
