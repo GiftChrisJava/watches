9167557dc82d6a97254f77d231adc13a65de6649
GiftChrisJava <chrisjava77@gmail.com>
2024-02-02 14:31:18 +0200

    creating edit watch

--- app/components/EditWatch.jsx
+++ app/components/EditWatch.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+"use client";
 
-export default function EditWatch() {
-  return <div>EditWatch</div>;
+import { useState } from "react";
+
+export default function EditWatch({ watch }) {
+  const [showModel, setShowModel] = useState(false);
+  const [formData, setFormData] = useState({
+    brand: watch.brand,
+    model: watch.model,
+    referenceNumber: watch.referenceNumber,
+  });
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <div>
+      <button onClick={() => setShowModel(true)}>Edit</button>
+      {showModel && (
+        <div>
+          <div>
+            <span onClick={() => setShowModel(false)}>&times;</span>
+            <form action="updateWatch" onSubmit={() => setShowModel(false)}>
+              <input type="hidden" name="id" id={watch.id} />
+
+              <div></div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
